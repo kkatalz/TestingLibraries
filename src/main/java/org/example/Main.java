0893518d1fd0d91dd0cd801f28c014cf47a086e4
kkatalz <zlata.kiev2004@gmail.com>
2024-06-26 12:07:32 +0300

    Added Assumptions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,11 @@ public class Main {
     }
 
     public static int add(int a, int b) {
-
         return a + b;
+    }
+
+
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 }
--- src/test/java/JUnitSimpleTest.java
+++ src/test/java/JUnitSimpleTest.java
@@ -1,6 +1,9 @@
 import org.example.Main;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assumptions;
 
 public class JUnitSimpleTest {
 
@@ -11,4 +14,19 @@ public class JUnitSimpleTest {
 
         assertEquals(result, 5, "2 + 3 should equal 5");
     }
+
+    @Test
+    void subtraction() {
+        // Припущення, що тест повинен виконуватися лише тоді, коли змінна середовища
+        // RUN_SUBTRACTION_TEST встановлена в true
+
+        boolean runTest = Boolean.parseBoolean(System.getenv("RUN_SUBTRACTION_TEST"));
+        Assumptions.assumeTrue(runTest, "Test skipped as RUN_SUBTRACTION_TEST is not true");
+
+        Main main = new Main();
+        int result = main.subtract(5, 3);
+        assertEquals(result, 2, "5 - 3 should equal 2");
+    }
+
+
 }
